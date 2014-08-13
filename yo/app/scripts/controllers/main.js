9997ec2193bc964de348eaf34d05ee53d5a95662
Alessandro Holanda <alessandro.holanda@hotmail.com>
2014-08-13 17:50:48 -0300

    fix wrong request in app.js

--- yo/app/scripts/controllers/main.js
+++ yo/app/scripts/controllers/main.js
@@ -5,17 +5,10 @@
  * @name yoApp.controller:MainCtrl
  * @description # MainCtrl Controller of the yoApp
  */
-angular.module('webApp').controller('MainCtrl', function($scope, $http) {
+angular.module('webApp').controller('MainCtrl', function($scope) {
 	$scope.awesomeThings = [ 'HTML5 Boilerplate', 'AngularJS', 'Karma' ];
 	$scope.serverSays = 'none';
     // Configura o subtítulo a ser usado na página.
-
-	$http.get('./rest/request/current').success(function(data) {
-		$scope.serverSays = data[0];
-		console.log($scope.serverSays);
-	}).error(function() {
-		$scope.serverSays = 'error !';
-	});
 
     var setTilesAreaSize = function(){
         var groups = $('.tile-group');
